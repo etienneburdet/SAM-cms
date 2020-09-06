0d13a75356204638743f74c3c1b7a6c3c9d62cae
Etienne Burdet <etienne.burdet@gmail.com>
2020-09-06 21:47:12 +0200

    Populate nested group in seance controller

--- api/seance/controllers/seance.js
+++ api/seance/controllers/seance.js
@@ -5,4 +5,8 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    return strapi.query('seance').find(ctx.query, ['trainings.workout.groupes'])
+  }
+}
